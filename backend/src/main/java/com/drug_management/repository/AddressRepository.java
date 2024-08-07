3c0bc33a918438e1ae5a6c583760cd280cd2ea39
Vishaal0812 <vishaal0812@gmail.com>
2024-08-07 10:26:53 +0530

    Revert "commit the drug management project"
    This reverts commit dcadd5e628c0d2139e42373b7f8a85ade021fb08.

--- backend/src/main/java/com/drug_management/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.drug_management.repository;
-
-import com.drug_management.modal.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- backend/src/main/java/com/drug_management/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.drug_management.repository;
-
-import com.drug_management.modal.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
